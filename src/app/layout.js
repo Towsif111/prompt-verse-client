08e36afb0aabd0b49580b6e4ce900f44505c154f
Towsif111 <towsifhassan399@gmail.com>
2026-06-20 19:31:45 +0600

    added the footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({ children }) {
       <body className="flex min-h-full flex-col bg-white text-slate-900">
         <Navbar />
         <main className="flex-1">{children}</main>
+        <Footer></Footer>
       </body>
     </html>
   );
